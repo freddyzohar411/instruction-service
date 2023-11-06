f121e27b45281f68ac8d93432243b0c6212af0bd
hxg-avensys <H.Xiang@aven-sys.com>
2023-11-06 13:50:17 +0800

    Switch user end point

--- src/main/java/com/avensys/rts/instructionservice/APIClient/UserAPIClient.java
+++ src/main/java/com/avensys/rts/instructionservice/APIClient/UserAPIClient.java
@@ -13,8 +13,8 @@ public interface UserAPIClient {
     @GetMapping("/{id}")
     HttpResponse getUserById(@PathVariable("id") Integer id);
 
-    @GetMapping("")
-    HttpResponse getUserByEmail(@RequestParam("email") String email);
+    @GetMapping("/email/{email}")
+    HttpResponse getUserByEmail(@PathVariable("email") String email);
 
 }
 
